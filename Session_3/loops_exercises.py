a5d3af9be0b623485fce0693a558578809f44fd5
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-06-10 14:36:30 +1000

    Adding in exercise questions for context

--- Session_3/loops_exercises.py
+++ Session_3/loops_exercises.py
@@ -1,4 +1,7 @@
-# # Continuously ask the user to enter a number until they provide a blank input. Outputthe sum of all the numbers.
+# While Loop Exercises
+# Q1 
+# Continuously ask the user to enter a number until they provide a blank input. Output
+# the sum of all the numbers.
 number = input("Enter a number: ")
 total = 0
 while number:
@@ -10,8 +13,9 @@ while number:
             break
 print(total)
 
-# Ask the user to enter a in integer number.
-# Print all the odd numbers between 0 and that number (inclusive).
+# Q2
+# Ask the user to enter a in integer number. Print all the odd numbers between 0 
+# and that number (inclusive).
 odd_numbers = []
 number = int(input("Enter a number: "))+1
 count = 0
@@ -23,10 +27,12 @@ while count < number:
             count+=1
 print(odd_numbers)
 
-# Write a guessing game.Select a number, and save it as a variable in your code.
-# Ask the user to enter a number, and then output whether their number is less than or greater than the
-# selected number. Keep asking until the user guesses the correct number.
-# Then print a congratulatatory message.
+# Q3
+# Write a guessing game.
+# Select a number, and save it as a variable in your code. Ask the user to enter a
+# number, and then output whether their number is less than or greater than the
+# selected number. Keep asking until the user guesses the correct number. Then print a 
+# congratulatatory message.
 import random
 
 secret_number = random.randint(0, 101)
@@ -50,27 +56,37 @@ while number_guess != secret_number:
                   print("I'm sorry, I do not understand. Please guess an integer. ")
 print(f"Congratulations! You guessed my secret number: {secret_number}!")
 
-# Ask the user for a number. Use a for loop to print the times tables for that number,up to ten
+
+# For Loop Exercises
+# Q1
+# Ask the user for a number. Use a for loop to print the times tables for that number,
+# up to ten
 multiplication_number = input("Enter a number: ")
 for i in range(1,11):
       print(f"{multiplication_number} * {i} = {int(multiplication_number) * i}")
 
-# Ask the user for an integer. Using a for loop, add up every number from 0 up to that integer, and print the result.
+# Q2
+# Ask the user for an integer. Using a for loop, add up every number from 0 up to that
+# integer, and print the result.
 number = int(input("Enter a number: "))
 total = 0
 for num in range(0,number +1):
       total += num
 print(total)
 
-# Save a list of numbers to a variable in your script,
-# and then use a for loop to print the sum of all the numbers in the list
+# Q3
+# Save a list of numbers to a variable in your script, and then use a for loop to print the 
+# sum of all the numbers in the list
 numbers = [3,5,8,10,7]
 total = 0
 for number in numbers:
       total+=number
 print(total)
 
-
+# Extension Exercise
+# Q1
+# Save a list of numbers to a variable in your script, and then use a for loop to print the
+# sum of all the numbers in the list.
 groceries = [
       ["Baby Spinach", 2.78],
       ["Hot Chocolate", 3.70],
@@ -85,6 +101,7 @@ for grocery in groceries:
       total += quantity
 print(round(total,2))
 
+# Q2
 # Let's improve the guessing game you wrote in Q3 of the while loop exercises. 
 # Update the code so that when the game ends, the program asks the player if they would 
 # like to play again. If they input "no", the game ends, but with any other input the game
