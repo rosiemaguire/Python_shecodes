2ccf60804e60cbef41a93e39dfd30afeb7d0d366
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-06-15 21:47:34 +1000

    Question 3 was tricky - don't be fooled by casing

--- Session_5/files_exercises.py
+++ Session_5/files_exercises.py
@@ -19,7 +19,6 @@ with open(file="colours_20_simple.csv") as csv_file:
 #     for row in reader:
 #         print (f"{list(row.values())[2]}, {list(row.keys())[1]}: {list(row.values())[1]}, {list(row.keys())[0]}: {list(row.values())[0]}")
 # # 
-import csv
 def read_csv(file_path):
     with open(file_path, 'r') as file:
         reader = csv.reader(file)
@@ -53,3 +52,35 @@ formatted_data = format_row_data('./colours_20_simple.csv')
 # Q3
 # Write a program that takes a csv file describing colours, and outputs the number of
 # times each of the following colours appears in the English Name
+# red, green, blue, yellow
+# first option only grabs full colour name if colour makes up entire string
+def solo_colour_string(file, colours):
+    with open(file) as file:
+        colours_2 = []
+        file = csv.reader(file)
+        for f in file:
+            colours_2+=(f[2].split())
+        colour_dict = {colour:0 for colour in colours}
+        lowercase_colours =[]
+        for colour in colours_2:
+            lowercase_colours.append(colour.lower())    
+        for colour in colours:
+            colour_dict[colour]=lowercase_colours.count(colour.lower())
+        return colour_dict
+# print(solo_colour_string("colours_865.csv",["red","green","blue","yellow"]))
+
+# second option grabs if colour name exists in string in list of colours
+def colour_exists_in_string (file,colours):
+    with open(file="colours_865.csv") as file:
+        colours_2 = []
+        file = csv.reader(file)
+        for f in file:
+            colours_2+=(f[2].split())
+        colour_dict = {colour:0 for colour in colours}
+        lowercase_colours =[]
+        for colour in colours_2:
+            lowercase_colours.append(colour.lower())    
+        for colour in colours:
+            colour_dict[colour]=sum(s.count(colour.lower()) for s in lowercase_colours)
+        return colour_dict
+# print(colour_exists_in_string("colours_865.csv",["red","green","blue","yellow"]))
